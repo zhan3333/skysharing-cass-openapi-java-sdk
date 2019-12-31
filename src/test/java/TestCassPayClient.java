41ca334f07fbb7dd6a93b1d03e7aeb2ccd9899ff
zhan <grianchan@gmail.com>
2019-12-31 16:24:43 +0800

    add env package

--- src/test/java/TestCassPayClient.java
+++ src/test/java/TestCassPayClient.java
@@ -7,6 +7,7 @@ import com.skysharing.api.exception.ResponseNotValidException;
 import com.skysharing.api.model.*;
 import com.skysharing.api.request.*;
 import com.skysharing.api.response.*;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -21,18 +22,28 @@ import static org.junit.Assert.*;
 @Ignore
 public class TestCassPayClient {
 
-    private String publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5rT49txD03KYuktQaaSic0efuGm9wlkTqWK4HQxZW6Tv4mvvnJBXScq4BI6URNy+L3ghqZrIQvGxtsR/VGiOXnSLguVl8tBZFtqChUenZz6Ur9thjP8TRTiBjlarHsfbWWyW34ZsmLlqVGx15YGtigsW0KLGJLblowBM2/rHxZ4BlWiOwkuTUlBhYUd8cKRMzvFaFYj4uuqlUGr6MZK3hv7is6CPSOwIt2+SSvrwPhs+1vmaQKiT2YoXb5qmiiLe0C+wOPrVfDls4RC8miLCCd90X7Bnvoi6e1wH4dXZuG38xSHFEt8LNmRNMsG90wrq1zGKni47Ila+NsGIz4h8HQIDAQAB";
-    private String privateKeyStr = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDmtPj23EPTcpi6S1BppKJzR5+4ab3CWROpYrgdDFlbpO/ia++ckFdJyrgEjpRE3L4veCGpmshC8bG2xH9UaI5edIuC5WXy0FkW2oKFR6dnPpSv22GM/xNFOIGOVqsex9tZbJbfhmyYuWpUbHXlga2KCxbQosYktuWjAEzb+sfFngGVaI7CS5NSUGFhR3xwpEzO8VoViPi66qVQavoxkreG/uKzoI9I7Ai3b5JK+vA+Gz7W+ZpAqJPZihdvmqaKIt7QL7A4+tV8OWzhELyaIsIJ33RfsGe+iLp7XAfh1dm4bfzFIcUS3ws2ZE0ywb3TCurXMYqeLjsiVr42wYjPiHwdAgMBAAECggEAJqC0crjMjnHIiqCjlRygqoaagokJ4amCdkD2LL7tkz+ZfqKt2tv8EXnkt7absq/3FAGcOUaWM0c+hyh9XUeoVr5SwZbhK/eggwgRBHnL2KiMwqkLu0zWECf24Ts3qY6y9lUNKd3a/vNEj2AdmVDOGqfiqQon/Ou2pUUemVJy8m5BwYLafp5tZk/Yvo5ns48I6recTaQiCTbwXlD0hSYVHhaOhtzIXLk5hjBKGasDNuvAmhqZM+XGnITTq4Z+wGZS3Ir4ynGZMn4FzzTXd41SZ/tucKjFmh3tSeCQjTBzKS+Y6je694UNCKy0TTFc9mANFs4qL+m58uD6KWUiRx/27QKBgQD/FgJQPB2W6Yw92vTS1fJA1irJLPeUoy3R99qDAlGjbnwht31943/DfhBtBGdEOUaXskJvtZLYwU4aE9CytUKQeqhuIU1uRosE44V3wq6EFTknXGFQ+XAT9e+hAjSn7HcHYjPRvxMJvmg3N/86oQAq/Zt6UNGmvCRwQzSdmdeUmwKBgQDniJm7v56xVuO/5W+P9yiNUayHgCfya7/eQij0Rw9hIJ0mKEUl2ENGGdtw6ZqsdrSD6yHkzGLTGCd0cy9cifwz25AjnSOf5kLHJem9WuxqcFlZQlF8X+ba1OOJwfQxyCrjhR2/1QttVGOl84kMDij/YYA0Ip+EQ5mP5YWxsfzRpwKBgGHX3VdNV8Q1HRf0zoe4jM2V2J+F4glfdgnd2jD6SLo5fN+p9Da/dphT2JUYZUvj9FHnjFgw+3ys2Ppjs19462ljwwtLNtofsN9VLjK7uBjt0xhn9OQMaZMeeKX3pJ7sstNgNaQ0eGb6ZBsaHYoti1TyJhsKuQANlqSBAsOY6PoVAoGAfpjIRkoeASVbXj+bYoJn3+16gpRmQzz7KhHOxtJb7GWix4xZUroO/rOAsxpoAkjdpwvX+nxuxLF+UUPx741bIxe4lmCDbBjBBpcKWQjwH7rSf+WtHG5rkde5mkc8uEOUf9b3Tz60AtGTsteYZckQTaMIMHmF8xHrUyzSjzic3h0CgYEApidFokFSKY39OLk9WqWLd+XfL1ykOJh4xsREADo09oIvVtRrtaojaDpCg6/ewb5eVHzIxutTJqrKNIiO00+jK9OBqHf3NIbVA2aAxj2+zTZCc/+zkqDjnhWFcKLU4hAH5j6mQrDcn7hzE5ic/nyILQOyO2hQVXqM82RFcIJPCkE=";
-    private String vzhuoPublicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzvaToLe+vIN/G4u9gxe3kL/myE129RsgZWIyTf7Tjc0rARdsvE9Xy2fdzNTwttyqJJaB9oNuCjdhNXAJoSUqoKei0aA88oFylPWG6DZ7ZoIXOGtipvnloosWhskes+E+EvQWrwSgzCAEr5N7iHY5DEBBy8ZoHsthd5CvCGyBgfeAsCCQ/R6Cl2Bw0SNasVu45+0PfmW/AUeC29i7KJm1HC/D5n6oXSa6q16oh6TWiFnVJF5dNCeuHN+3KQAe/Hv3CldiTHB8mND9OYzx2DzZYJD7g8f185Td5a8mRwlYf2PwTfGLkMdxa+XlGIsHjCFbXD1N85XC3QVfbgZWnxQA9QIDAQAB";
-    private String url = "http://fuwu-openapi.skysharing.cn/gateway/cass";
-    private String APPID = "12345H29GDK580CGNO30G81OEB";
-
+    private String privateKeyStr;
+    private String vzhuoPublicKeyStr;
+    private String url;
+    private String APPID;
+    private Dotenv dotenv;
     private DefaultCassPayClient client;
 
     @Before
     public void init() throws InvalidPrivateKeyException, InvalidPublicKeyException {
         System.out.println("start test");
+        this.dotenv = Dotenv.load();
+        this.privateKeyStr = dotenv.get("PRIVATE_KEY_STR");
+        this.vzhuoPublicKeyStr = dotenv.get("VZHUO_PUBLIC_KEY_STR");
+        this.APPID = dotenv.get("APPID");
+        this.url = dotenv.get("API_URL");
         this.client = new DefaultCassPayClient(this.url, this.APPID, this.privateKeyStr, this.vzhuoPublicKeyStr, "JSON", "RSA2");
+    }
+
+    @Test
+    public void testGetEnv() {
+        String APPID = dotenv.get("APPID");
+        System.out.println(APPID);
     }
 
     @Test
